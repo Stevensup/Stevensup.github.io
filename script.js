7b248941dc31416fb15ac991e6866497748ccc4c
Stevenup <114196322+Stevensup@users.noreply.github.com>
2024-06-09 20:02:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,111 +1,310 @@
-function actualizarContador() {
-    // Fecha objetivo (24 de abril)
-    const fechaObjetivo = new Date("2024-04-24T10:50:00");
+var preguntas = [
+  {
+    pregunta:
+      "La proteína Math1 (factor de transcripción específico) tiene un dominio de unión a ADN hélix-loophélix por ello:",
+    opciones: [
+      "Presenta en su secuencia, regiones que codifican a los aminoácidos que funcionarán como señal de localización nuclear y su síntesis ocurre en polirribosomas libres.",
+      "Durante su procesamiento en el RER y Golgi, se agregarán modificaciones postraduccionales que permitirán su ingreso al núcleo.",
+      "Presenta en su secuencia, regiones que codifican a los aminoácidos que funcionarán como péptido señal para su síntesis en ribosomas asociados al RER.",
+      "Presenta en la estructura final proteica, aminoácidos que funcionarán como señal de localización nuclear producto de modificaciones postraduccionales en el citosol",
+    ],
+    respuestaCorrecta:
+      "Presenta en su secuencia, regiones que codifican a los aminoácidos que funcionarán como señal de localización nuclear y su síntesis ocurre en polirribosomas libres.",
+  },
 
-    // Fecha y hora actual
-    const fechaActual = new Date();
+  {
+    pregunta:
+      "La alteración del proceso de invasión trofoblástica puede desencadenar preeclampsia. Esta es una patología que aumenta la mortalidad materna. Un factor descrito habitualmente en su patogénesis, es el aumento de la secreción trofoblástica de una proteína inhibidora de VEGF (factor de crecimiento del endotelio vascular), denominada sFLT-1. La síntesis de sFLT-1 implica:",
+    opciones: [
+      "La traducción de un péptido señal codificado en el transcripto maduro.",
+      "El agregado de manosa-6-fosfato en el aparato de Golgi.",
+      "La finalización de la transcripción en el REG.",
+      "El plegamiento de la proteína a través de chaperonas citosólicas.",
+    ],
+    respuestaCorrecta:
+      "La traducción de un péptido señal codificado en el transcripto maduro.",
+  },
 
-    // Diferencia en milisegundos entre la fecha actual y la fecha objetivo
-    const diferencia = fechaObjetivo - fechaActual;
+  {
+    pregunta:
+      "La capacidad invasiva de las células neoplásicas malignas se ve favorecida por su capacidad para remodelar la matriz extracelular mediante la síntesis y actividad de proteínas metaloproteasas de la matriz extracelular. En este proceso participan los siguientes mecanismos moleculares:",
+    opciones: [
+      "Síntesis proteica en el retículo endoplasmático rugoso (REG), modificaciones en REG y aparato de Golgi; y transporte por la vía secretora.",
+      "Síntesis en membrana plasmática, internalización mediada por receptor y transporte retrógrado hacia el cuerpo celular.",
+      "Inicio de síntesis proteica en ribosomas libres, su terminación en el citosol y secreción por canales de la membrana plasmática.",
+      "Síntesis proteica en el retículo endoplasmático rugoso (REG), modificaciones en el aparato de Golgi, y transporte por la vía de degradación endo-lisosomal.",
+    ],
+    respuestaCorrecta:
+      "Síntesis proteica en el retículo endoplasmático rugoso (REG), modificaciones en REG y aparato de Golgi; y transporte por la vía secretora.",
+  },
 
-    // Convertir la diferencia a días, horas, minutos y segundos
-    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-    const horas = Math.floor(
-      (diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutos = Math.floor(
-      (diferencia % (1000 * 60 * 60)) / (1000 * 60)
-    );
-    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+  {
+    pregunta:
+      "¿Cuál de los siguientes procesos de maduración de una glicoproteína de exportación ocurre durante su paso por la vía secretora?",
+     opciones: [
+        "Plegamiento en el Aparato de Golgi por ubiquitinación.",
+        "Glicosilación en el retículo endoplasmático rugoso (RER).",
+        "Unión a endosomas.",
+        "Anclaje a la membrana del retículo endoplasmático liso (REL)."
+    ],
+    respuestaCorrecta: "Glicosilación en el retículo endoplasmático rugoso (RER)."
+  },
 
-      // Mostrar el contador en el elemento HTML
-  const contadorElement = document.getElementById("contador");
-  contadorElement.innerHTML = `
-    <span>FELIZ CUMPLEAÑOS</span>
-    
-`;
-}
+  {
+    pregunta:
+      "La autofagia es un evento celular que ocurre ante diferentes situaciones. Señale cuál de las siguientes opciones es la correcta respecto a este proceso:",
+    opciones: [
+        "Los productos de la degradación de las biomoléculas no son reciclados.",
+        "Las mitocondrias disfuncionales no pueden ser eliminadas por este proceso.",
+        "Permite eliminar proteínas mal plegadas.",
+        "La digestión se da en los peroxisomas."
+    ],
+    respuestaCorrecta: "Permite eliminar proteínas mal plegadas."
+  },
 
+  {
+    pregunta:
+      "Una mutación provoca que la proteína de transmembarna adquiera una conformación mal plegada. Como consecuencia del mal plegamiento, la proteína es:",
+    opciones: [
+        "poliubiquitinizada luego de su síntesis en ribosomas libres",
+        "transportada para su degradación al lisosoma por medio de la señal de manosa 6 fosfato",
+        "secretada porque carece de partícula de reconocimiento (PRS) de asociación a Retículo Endoplasmático Liso.",
+        "es retenida en el Retículo Endoplasmático Rugoso y enviada a degradación."
+    ],
+    respuestaCorrecta: "es retenida en el Retículo Endoplasmático Rugoso y enviada a degradación."
+  },
 
-// Actualizar el contador cada segundo
-setInterval(actualizarContador, 1000);
+  {
+    pregunta:
+      "Un mecanismo de enfermedad asociado a proteínas de importación nuclear implica la acumulación de las mismas en el citosol debido a:",
+    opciones: [
+        "Una mayor asociación de ARN mensajeros inmaduros con exportinas para salir del núcleo a través de los poros nucleares.",
+        "Una menor asociación de ARN mensajeros maduros con exportinas para salir del núcleo a través de los poros nucleares.",
+        "Una mayor asociación de estas proteínas a las importinas/Ran-GTP para entrar al núcleo a través de los poros nucleares.",
+        "Una menor asociación de estas proteínas a las importinas/Ran-GTP para entrar al núcleo a través de los poros nucleares."
+    ],
+    respuestaCorrecta: "Una menor asociación de estas proteínas a las importinas/Ran-GTP para entrar al núcleo a través de los poros nucleares."
+  },
 
-// Inicializar el contador al cargar la página
-actualizarContador();
+  {
+    pregunta:
+      "La distribución de las mitocondrias en proyecciones distantes del cuerpo celular (por ej en el axón de una neurona) es generado mediante:",
+    opciones: [
+        "Proteínas motoras kinesinas que se desplazan asociadas a los microtúbulos.",
+        "Proteínas motoras dineínas que se desplazan asociadas a microfilamentos.",
+        "Difusión en base a distintas concentraciones de mitocondrias.",
+        "Proteínas motoras kinesinas que se desplazan asociadas a filamentos intermedios."
+    ],
+    respuestaCorrecta: "Proteínas motoras kinesinas que se desplazan asociadas a los microtúbulos."
+  },
 
-function mostrarMensaje(opcion) {
-    if (opcion === "eres mi novia") {
-      alert("¡Eres mi novia! ¡Te amo!");
-    } else if (opcion === "invitado") {
-        window.open("invitado.png", "_blank");
-        
-    }
-}
-
-// Funciones para el modal
-const modal = document.getElementById('myModal');
-const modalContent = document.querySelector('.modal-content');
-
-function abrirModal() {
-    modal.style.display = 'block';
-    document.getElementById('modalImagen').src = 'menu.png';
-
-}
-
-function cerrarModal() {
-    modal.style.display = 'none';
-}
-
-// Cambio de fondo del modal
-let contadorFondo = 0;
-const fondos = ['#ff5733', '#33ff57', '#3333ff', '#ff33f9', '#f9ff33'];
-
-function cambiarFondo() {
-    contadorFondo = (contadorFondo + 1) % fondos.length;
-    modalContent.style.backgroundColor = fondos[contadorFondo];
-}
-
-// Imágenes y textos para el modal
-const motivos = [
-    {
-        imagen: 'imagen1.png',
-        texto: 'Motivo 1: Creatividad y originalidad en tus proyectos.'
-    },
-    {
-        imagen: 'imagen2.png',
-        texto: 'Motivo 2: Sensibilidad y pasión por el arte y el diseño.'
-    },
-    {
-        imagen: 'imagen3.png',
-        texto: 'Motivo 3: Visión única para resolver problemas visuales.'
-    },
-    {
-        imagen: 'imagen4.png',
-        texto: 'Motivo 4: Capacidad de comunicar ideas de forma impactante.'
-    },
-    {
-        imagen: 'imagen5.png',
-        texto: 'Motivo 5: Aporta belleza y estética a cualquier proyecto.'
-    }
+  {
+    pregunta:
+      "Señale la opción correcta respecto de las proteínas de cubierta vesicular (clatrina, COP I, COP II)",
+    opciones: [
+        "Seleccionan el compartimento de destino de las vesículas.",
+        "Hidrolizan el ATP necesario para el transporte de las vesículas sobre los microtúbulos.",
+        "Participan en el proceso de gemación vesicular.",
+        "Permiten la fusión de la membrana de la vesícula con el compartimento de destino."
+    ],
+    respuestaCorrecta: "participan en el proceso de gemación vesicular."
+  },
+  {
+    pregunta: "El transporte de vesículas:",
+    opciones: [
+        "Es bidireccional (+/-) y permite el tránsito de vesículas de secreción y vesículas endocíticas a través de filamentos de actina.",
+        "Es unidireccional y genera el transporte anterógrado de vesículas de secreción hacia el extremo + de los microtúbulos.",
+        "Es bidireccional y permite el tránsito de vesículas de secreción, vesículas endocíticas y entre organelas a través de microtúbulos.",
+        "Es bidireccional y permite el tránsito de vesículas de secreción, vesículas endocíticas y entre organelas a través de filamentos intermedios."
+    ],
+    respuestaCorrecta: "Es bidireccional y permite el tránsito de vesículas de secreción, vesículas endocíticas y entre organelas a través de microtúbulos."
+  },
 ];
 
-const modalImagen = document.getElementById('modalImagen');
-const modalTexto = document.getElementById('modalTexto');
-const nextButton = document.getElementById('nextButton');
 
-let contadorMotivos = 0;
+var nombreUsuario = "";
+var preguntaActual = 0;
+var puntuacion = 0;
+var respuestasUsuario = [];
+var tiempoPorPregunta = 30000; // tiempo por defecto: 30 segundos
+var temporizador;
+var startTime;
 
-function mostrarSiguienteMotivo() {
-    contadorMotivos = (contadorMotivos + 1) % motivos.length;
-    modalImagen.src = motivos[contadorMotivos].imagen;
-    modalTexto.textContent = motivos[contadorMotivos].texto;
+
+function iniciarQuiz(event) {
+    event.preventDefault();
+    var nombreInput = document.getElementById("nombreInput");
+    nombreUsuario = nombreInput.value.trim();
+    var velocidadSelect = document.getElementById("velocidadSelect");
+    tiempoPorPregunta = parseInt(velocidadSelect.value);
+
+    startTime = new Date().getTime();
+
+    document.getElementById("nombreContainer").style.display = "none";
+    document.getElementById("quizContainer").style.display = "block";
+    mostrarPregunta();
+    iniciarTemporizador();
 }
 
-nextButton.addEventListener('click', mostrarSiguienteMotivo);
+function mostrarPregunta() {
+    var pregunta = preguntas[preguntaActual];
+    document.getElementById("pregunta").textContent = "Pregunta " + (preguntaActual + 1) + " de " + preguntas.length + ": " + pregunta.pregunta;
+    document.getElementById("contadorTiempo").textContent = "Tiempo seleccionado: " + (tiempoPorPregunta / 1000) + " segundos";
+
+    var opciones = document.getElementById("opciones");
+    opciones.innerHTML = "";
+    for (var i = 0; i < pregunta.opciones.length; i++) {
+        var opcion = document.createElement("input");
+        opcion.type = "radio";
+        opcion.name = "opcion";
+        opcion.value = pregunta.opciones[i];
+        opcion.id = "opcion" + i;
+        opcion.required = true; // Hace que la respuesta sea obligatoria
+
+        var etiqueta = document.createElement("label");
+        etiqueta.htmlFor = "opcion" + i;
+        etiqueta.textContent = pregunta.opciones[i];
+
+        opciones.appendChild(opcion);
+        opciones.appendChild(etiqueta);
+        opciones.appendChild(document.createElement("br"));
+    }
+}
+
+function comprobarRespuesta(event) {
+    event.preventDefault();
+
+    detenerTemporizador();
+
+    var opciones = document.getElementsByName("opcion");
+    var respuestaUsuario = "";
+    for (var i = 0; i < opciones.length; i++) {
+        if (opciones[i].checked) {
+            respuestaUsuario = opciones[i].value;
+            break;
+        }
+    }
+
+    respuestasUsuario.push({
+        pregunta: preguntas[preguntaActual].pregunta,
+        respuestaCorrecta: preguntas[preguntaActual].respuestaCorrecta,
+        respuestaUsuario: respuestaUsuario,
+    });
+
+    if (respuestaUsuario == preguntas[preguntaActual].respuestaCorrecta) {
+        puntuacion++;
+    }
+
+    preguntaActual++;
+    if (preguntaActual < preguntas.length) {
+        mostrarPregunta();
+        iniciarTemporizador(); // Iniciar el temporizador para la próxima pregunta
+    } else {
+        finalizarQuiz();
+    }
+}
+
+function iniciarTemporizador() {
+    var tiempoRestante = tiempoPorPregunta / 1000;
+    document.getElementById("contadorTiempo").textContent = "Tiempo restante: " + tiempoRestante + " segundos";
+
+    temporizador = setInterval(function () {
+        tiempoRestante--;
+        document.getElementById("contadorTiempo").textContent = "Tiempo restante: " + tiempoRestante + " segundos";
+
+        if (tiempoRestante <= 0) {
+            detenerTemporizador(); // Detener el temporizador actual
+            comprobarRespuesta({ preventDefault: function() {} }); // Pasar un evento simulado
+        }
+    }, 1000);
+}
 
 
-function mostrarImagen(imagen) {
-    var nuevaVentana = window.open("", "_blank");
-    nuevaVentana.document.write('<html><head><title>Imagen</title></head><body style="margin: 0; background-color: #000;"><img src="' + imagen + '" style="max-width: 100%; max-height: 100vh; display: block; margin: 0 auto;"></body></html>');
-    nuevaVentana.document.close();
+function detenerTemporizador() {
+    clearInterval(temporizador);
+}
+
+function finalizarQuiz() {
+    detenerTemporizador();
+    var endTime = new Date().getTime();
+    var elapsedTime = endTime - startTime;
+    var seconds = Math.floor(elapsedTime / 1000);
+    var minutes = Math.floor(seconds / 60);
+    seconds %= 60;
+
+    var tiempoElemento = document.getElementById("tiempo");
+    if (tiempoElemento) {
+        tiempoElemento.textContent = "Te tomó " + minutes + " minutos " + seconds + " segundos en responder todas las preguntas.";
+    } else {
+        console.error("Elemento 'tiempo' no encontrado.");
+    }
+
+    var puntuacionElemento = document.getElementById("puntuacion");
+    if (puntuacionElemento) {
+        puntuacionElemento.textContent =
+            "Puntuación: " +
+            puntuacion +
+            "/" +
+            preguntas.length +
+            " (Correctas: " +
+            puntuacion +
+            ", Incorrectas: " +
+            (preguntas.length - puntuacion) +
+            ")";
+    } else {
+        console.error("Elemento 'puntuacion' no encontrado.");
+    }
+
+    var resumen = document.getElementById("resumen");
+    if (resumen) {
+        resumen.innerHTML = "";
+        for (var i = 0; i < respuestasUsuario.length; i++) {
+            // Create list items for each response
+            var item = document.createElement("li");
+            item.textContent =
+                "Pregunta: " +
+                respuestasUsuario[i].pregunta +
+                " - Respuesta correcta: " +
+                respuestasUsuario[i].respuestaCorrecta +
+                " - Tu respuesta: " +
+                (respuestasUsuario[i].respuestaUsuario || "No respondida");
+            if (
+                respuestasUsuario[i].respuestaUsuario ==
+                respuestasUsuario[i].respuestaCorrecta
+            ) {
+                item.style.color = "green";
+            } else {
+                item.style.color = "red";
+            }
+            resumen.appendChild(item);
+        }
+    } else {
+        console.error("Elemento 'resumen' no encontrado.");
+    }
+
+    // Display the result container
+    var resultadoContainer = document.getElementById("resultado");
+    if (resultadoContainer) {
+        resultadoContainer.style.display = "block";
+    } else {
+        console.error("Elemento 'resultado' no encontrado.");
+    }
+
+    // Hide the quiz container
+    var quizContainer = document.getElementById("quizContainer");
+    if (quizContainer) {
+        quizContainer.style.display = "none";
+    } else {
+        console.error("Elemento 'quizContainer' no encontrado.");
+    }
+}
+
+function reiniciarQuiz() {
+    preguntaActual = 0;
+    puntuacion = 0;
+    respuestasUsuario = [];
+    document.getElementById("quizForm").reset(); // Resetea el formulario de respuestas
+    document.getElementById("nombreContainer").style.display = "block"; // Muestra el formulario de nombre
+    document.getElementById("quizContainer").style.display = "none";
+    document.getElementById("resultado").style.display = "none";
 }
